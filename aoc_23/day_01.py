5b8a1b819c7c2b44db8309db7e28d4323bd37613
Joseph Gulay <joegulay@gmail.com>
2023-12-01 20:20:17 +1100

    Create day_01 test file

---
+++ aoc_23/day_01.py
@@ -0,0 +1,16 @@
+from load_data import get_data
+from itertools import combinations
+day = 01
+
+## Using 2020 Day 1 as an example
+## https://adventofcode.com/2020/day/1
+
+def part_1(data):
+    pass
+
+# ### Uncomment the lines below when your function passes the test!
+# raw_data = get_data(day)
+# print(f'part 1 solution = {part_1(raw_data)}')
+
+# def part_2(data):
+#     pass
