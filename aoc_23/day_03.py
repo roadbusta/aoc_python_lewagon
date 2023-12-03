2a077f7ab3332f704938eb7489aa29f51c8cd8ef
Joseph Gulay <joegulay@gmail.com>
2023-12-03 18:16:42 +1100

    Create template for day 3

---
+++ aoc_23/day_03.py
@@ -0,0 +1,33 @@
+from load_data import get_data,get_test_data
+import re
+import numpy as np
+day = 3
+# Convert data into array 2d array
+# Write a function that identifies numbers, and returns their array position
+# Write a function that checks around the number for another symbol
+
+def str_to_array(data: str) -> np.array:
+    pass
+
+
+def part_1(data: str) -> int:
+    pass
+
+def part_2(data: str) ->int:
+    pass
+
+
+## Uncomment the lines below when your function passes the test!
+real_data = get_data(day)
+
+print(f'part 1 solution = {part_1(real_data)}')
+print(f'part 2 solution = {part_2(real_data)}')
+
+if __name__ == "__main__":
+
+    # ## Uncomment the lines below when your function passes the test!
+    # real_data = get_test_data(day)
+    # part_1(real_data)
+    # part_2(real_data)
+
+    pass
