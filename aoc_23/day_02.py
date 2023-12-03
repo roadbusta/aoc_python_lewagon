bf7ad3d09a54e055f164ae95a26dc870ed5cc5c1
Joseph Gulay <joegulay@gmail.com>
2023-12-03 18:07:00 +1100

    Complete day_02.py file

--- aoc_23/day_02.py
+++ aoc_23/day_02.py
@@ -81,16 +81,16 @@ def part_2(data: str) ->int:
 
 
 ## Uncomment the lines below when your function passes the test!
-# real_data = get_data(day)
+real_data = get_data(day)
 
-# print(f'part 1 solution = {part_1(real_data)}')
-# print(f'part 2 solution = {part_2(real_data)}')
+print(f'part 1 solution = {part_1(real_data)}')
+print(f'part 2 solution = {part_2(real_data)}')
 
 if __name__ == "__main__":
 
     # ## Uncomment the lines below when your function passes the test!
     real_data = get_test_data(day)
-    # print(part_1(real_data))
+    part_1(real_data)
     part_2(real_data)
 
     pass
