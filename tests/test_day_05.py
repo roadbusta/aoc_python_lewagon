513b59c38b8cdc12f61c41a42a941ee0c71573e7
Joseph Gulay <joegulay@gmail.com>
2023-12-05 07:44:42 +1100

    Update template files and also day 05.py template files

---
+++ tests/test_day_05.py
@@ -0,0 +1,16 @@
+# from aoc_23.day_xx import
+from aoc_23.load_data import get_test_data
+import numpy as np
+
+
+test_data = get_test_data(xx)
+expected_result_part_1 = None
+expected_result_part_2 = None
+
+
+def test_part_1():
+    assert type(test_data) == str
+
+
+def test_part_2():
+   pass
