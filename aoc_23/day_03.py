42e6729bd824913a1e4b44a2979cd3707cbaa093
Joseph Gulay <joegulay@gmail.com>
2023-12-04 18:52:55 +1100

    Added some code for day_03.py, will come back to this later

--- aoc_23/day_03.py
+++ aoc_23/day_03.py
@@ -40,7 +40,7 @@ def num_position(machine_array: np.ndarray) -> list:
 
 def find_valid_adj(machine_array,i,j):
     """
-    Does a search around i and j and returns True if found
+    Does a search around i and j and returns a True if it is a valid adjacent
     """
     shape = machine_array.shape
     valid_adj = False
@@ -68,6 +68,215 @@ def find_valid_num(machine_array,valid_array,i,j):
                 valid_num = True
     return valid_num
 
+def num_adj_gear(machine_array,i,j):
+    """
+    Returns the valid array containing valid gears
+    """
+    # Set adj_num_count
+    adj_num_count = 0
+    shape = machine_array.shape
+    # Check top row
+    a = 0
+    alpha = i-1
+    beta = j-1
+    if alpha > -1:
+        if beta> -1:
+            char = str(machine_array[alpha,beta])
+            if char.isdigit():
+                a = 1
+
+    b = 0
+    alpha = i-1
+    beta = j
+    if alpha > -1:
+        char = str(machine_array[alpha,beta])
+        if char.isdigit():
+            b = 1
+
+    c = 0
+    alpha = i-1
+    beta = j+1
+    if alpha > -1:
+        if beta < shape[1]:
+            char = str(machine_array[alpha,beta])
+            if char.isdigit():
+                c = 1
+    # Evaluate top row
+    if a == 1 and b == 0 and c == 1:
+        adj_num_count +=2
+
+    elif a == 0 and b == 0 and c == 0:
+        adj_num_count += 0
+
+    else:
+        adj_num_count += 1
+
+    # Evaluate left
+    alpha = i
+    beta = j+1
+    if beta > -1:
+        char = str(machine_array[alpha,beta])
+        if char.isdigit():
+            adj_num_count +=1
+
+    # Evaluate right
+    alpha = i
+    beta = j-1
+    if beta < shape[1]:
+        char = str(machine_array[alpha,beta])
+        if char.isdigit():
+            adj_num_count +=1
+
+    # Evaluate bottom
+
+    d = 0
+    alpha = i+1
+    beta = j-1
+    if alpha < shape[0]:
+        if beta> -1:
+            char = str(machine_array[alpha,beta])
+            if char.isdigit():
+                d = 1
+
+    e = 0
+    alpha = i+1
+    beta = j
+    if alpha < shape[0]:
+        char = str(machine_array[alpha,beta])
+        if char.isdigit():
+            e = 1
+
+    f = 0
+    alpha = i+1
+    beta = j+1
+    if alpha < shape[0]:
+        if beta < shape[1]:
+            char = str(machine_array[alpha,beta])
+            if char.isdigit():
+                f = 1
+    # Evaluate top row
+    if d == 1 and e == 0 and f == 1:
+        adj_num_count +=2
+
+    elif d == 0 and e == 0 and f == 0:
+        adj_num_count += 0
+
+    else:
+        adj_num_count += 1
+
+    return adj_num_count
+
+def num_adj_gear(machine_array,i,j):
+    """
+    Returns the valid array containing valid gears
+    """
+    # Set adj_num_count
+    adj_num_count = 0
+    shape = machine_array.shape
+    # Check top row
+    a = 0
+    alpha = i-1
+    beta = j-1
+    if alpha > -1:
+        if beta> -1:
+            char = str(machine_array[alpha,beta])
+            if char.isdigit():
+                a = 1
+
+    b = 0
+    alpha = i-1
+    beta = j
+    if alpha > -1:
+        char = str(machine_array[alpha,beta])
+        if char.isdigit():
+            b = 1
+
+    c = 0
+    alpha = i-1
+    beta = j+1
+    if alpha > -1:
+        if beta < shape[1]:
+            char = str(machine_array[alpha,beta])
+            if char.isdigit():
+                c = 1
+    # Evaluate top row
+    if a == 1 and b == 0 and c == 1:
+        adj_num_count +=2
+
+    elif a == 0 and b == 0 and c == 0:
+        adj_num_count += 0
+
+    else:
+        adj_num_count += 1
+
+    # Evaluate left
+    alpha = i
+    beta = j+1
+    if beta > -1:
+        char = str(machine_array[alpha,beta])
+        if char.isdigit():
+            adj_num_count +=1
+
+    # Evaluate right
+    alpha = i
+    beta = j-1
+    if beta < shape[1]:
+        char = str(machine_array[alpha,beta])
+        if char.isdigit():
+            adj_num_count +=1
+
+    # Evaluate bottom
+
+    d = 0
+    alpha = i+1
+    beta = j-1
+    if alpha < shape[0]:
+        if beta> -1:
+            char = str(machine_array[alpha,beta])
+            if char.isdigit():
+                d = 1
+
+    e = 0
+    alpha = i+1
+    beta = j
+    if alpha < shape[0]:
+        char = str(machine_array[alpha,beta])
+        if char.isdigit():
+            e = 1
+
+    f = 0
+    alpha = i+1
+    beta = j+1
+    if alpha < shape[0]:
+        if beta < shape[1]:
+            char = str(machine_array[alpha,beta])
+            if char.isdigit():
+                f = 1
+    # Evaluate top row
+    if d == 1 and e == 0 and f == 1:
+        adj_num_count +=2
+
+    elif d == 0 and e == 0 and f == 0:
+        adj_num_count += 0
+
+    else:
+        adj_num_count += 1
+
+    return adj_num_count
+
+
+def find_gear(machine_array: np.ndarray) -> np.ndarray:
+     # create an empty array same size of machine_array
+    gear_array = np.full(machine_array.shape, '')
+    for i in range(len(machine_array)): # For each row
+        for j in range(len(machine_array[i])): # For each item in the row
+            # Check if it is a number
+            char = str(machine_array[i,j]) # Look at the character
+            if char == '*': # If it's a digit
+                if num_adj_gear(machine_array,i,j) ==2:
+                    gear_array[i,j] = '*'
+    return gear_array
+
 def valid_array(machine_array: np.ndarray) -> np.ndarray:
     """
     Takes in the machine as an array and returns a validated array where a 1
@@ -82,7 +291,7 @@ def valid_array(machine_array: np.ndarray) -> np.ndarray:
             # Check if it is a number
             char = str(machine_array[i,j]) # Look at the character
             if char.isdigit(): # If it's a digit
-                if find_valid_adj(machine_array,i,j):
+                if find_valid_adj(machine_array,i,j): #
                     valid_array[i,j] = machine_array[i,j]
 
     return valid_array
@@ -113,17 +322,11 @@ def valid_number_group_iter(machine_array: np.ndarray, valid_array: np.ndarray) 
     return valid_array
 
 
-
 def part_1(data: str) -> int:
     machine_array = str_to_array(data)
-    # print(machine_array)
-    # num_position_list = num_position(machine_array)
+
     validated_array = valid_array(machine_array)
-    # print(validated_array)
-    # validated_array_2 = valid_number_group(machine_array, validated_array)
-    # print(validated_array_2)
-    # validated_array_3 = valid_number_group(machine_array, validated_array_2)
-    # print(validated_array_3)
+
     final_array = valid_number_group_iter(machine_array,validated_array)
 
     flatten_list = list(chain.from_iterable(final_array))
@@ -140,31 +343,14 @@ if __name__ == "__main__":
 
     # ## Uncomment the lines below when your function passes the test!
     # data = get_test_data(day)
-    # machine_array = str_to_array(data)
-    # # print(machine_array)
-    # # num_position_list = num_position(machine_array)
-    # validated_array = valid_array(machine_array)
-    # # print(validated_array)
-    # # validated_array_2 = valid_number_group(machine_array, validated_array)
-    # # print(validated_array_2)
-    # # validated_array_3 = valid_number_group(machine_array, validated_array_2)
-    # # print(validated_array_3)
-    # final_array = valid_number_group_iter(machine_array,validated_array)
 
-    # flatten_list = list(chain.from_iterable(final_array))
-    # long_string = "".join(flatten_list).split(',')
-    # final_list = [int(i) for i in long_string if i]
-    # print(final_list)
-    # print(sum(final_list))
-    # num_group(num_position_list)
-    # part_1(real_data)
-    # part_2(real_data)
-    # print(part_1(data))
 
     ## Uncomment the lines below when your function passes the test!
-    real_data = get_data(day)
-
-    print(f'part 1 solution = {part_1(real_data)}')
+    data = get_test_data(day)
+    machine_array = str_to_array(data)
+    # print(f'part 1 solution = {part_1(real_data)}')
     # print(f'part 2 solution = {part_2(real_data)}')
+
+    print(find_gear(machine_array))
 
     pass
