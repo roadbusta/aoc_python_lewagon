8eb8e59ea1fc9c3e0dd502fbdba195dce8042ef8
Joseph Gulay <joegulay@gmail.com>
2023-12-05 20:47:28 +1100

    Create part 1 solution that passes all tests for day 5

--- aoc_23/day_05.py
+++ aoc_23/day_05.py
@@ -63,9 +63,50 @@ def _parse_source_data(data: str) -> dict:
 
     return parsed_dict
 
+def _destination(source_num: int, list_dict:list) -> int:
+    """Takes in a source value and mapping list of dictionaries as an integer and returns the destination value as
+    an integer
+    """
+    dest_num = source_num
+    for sub_dict in list_dict:
+        source = sub_dict['source']
+        dest = sub_dict['dest']
+        if source_num in range(source, source + sub_dict['range_len']):
+            delta = source_num - source
+            dest_num = dest + delta
+        else:
+            continue
 
-def part_1():
-    pass
+    return dest_num
+
+def _final_destination(seed_num: int, parsed_data:dict) -> int:
+    """ Takes in source number and parsed_data_dict and returns the final
+    destination_number for the source number
+    """
+    dest_1 = _destination(seed_num, parsed_data['seed-to-soil'])
+    dest_2 = _destination(dest_1, parsed_data['soil-to-fertilizer'])
+    dest_3 = _destination(dest_2, parsed_data['fertilizer-to-water'])
+    dest_4 = _destination(dest_3, parsed_data['water-to-light'])
+    dest_5 = _destination(dest_4, parsed_data['light-to-temperature'])
+    dest_6 = _destination(dest_5, parsed_data['temperature-to-humidity'])
+
+    return _destination(dest_6, parsed_data['humidity-to-location'])
+
+def part_1(data:str) -> int:
+    """ Takes in the test data then returns the lowest location
+    """
+
+    # Parse the data
+    parsed_data = _parse_source_data(data)
+
+    # Generate seed list
+    seeds = parsed_data['seeds']
+
+    # Generate destition list
+    dest_list = [_final_destination(seed_num, parsed_data) for seed_num in seeds]
+
+    return min(dest_list) # Return the minimum value
+
 
 def part_2():
     pass
@@ -74,9 +115,13 @@ def part_2():
 if __name__ == "__main__":
 
     ## Uncomment the lines below when your function passes the test!
-    data = get_test_data(day)
-    _parse_source_data(data)
-    # print(f'part 1 solution = {part_1(real_data)}')
+    # data = get_test_data(day)
+    real_data = get_data(day)
+    # parsed_data = _parse_source_data(data)
+    # test_mapping_dict = [{'dest' : 50, 'source' : 98, 'range_len' : 2}]
+    # _destination(99, test_mapping_dict)
+
+    print(f'part 1 solution = {part_1(real_data)}')
     # print(f'part 2 solution = {part_2(real_data)}')
 
 
--- tests/test_day_05.py
+++ tests/test_day_05.py
@@ -1,10 +1,11 @@
-from aoc_23.day_05 import _parse_source_data, _positive_int
+from aoc_23.day_05 import _parse_source_data, _positive_int, _destination, part_1
 from aoc_23.load_data import get_test_data
 import numpy as np
 
 
 test_data = get_test_data(5)
 parsed_data = _parse_source_data(test_data)
+test_mapping_dict =[ {'dest' : 50, 'source' : 98, 'range_len' : 2}]
 seeds = parsed_data['seeds']
 seed_soil = parsed_data['seed-to-soil']
 soil_fert = parsed_data['soil-to-fertilizer']
@@ -31,6 +32,12 @@ def test_part_1():
 
     assert type(soil_fert) == list # Confirm data type of seeds list
     assert _positive_int([soil_fert[0]['dest']]) == True # Confirm that data types are positive int
+
+    assert _destination(1,test_mapping_dict) == 1
+    assert _destination(99, test_mapping_dict) == 51
+
+    assert type(part_1(test_data)) == int
+    assert part_1(test_data) == expected_result_part_1
 
 
 
