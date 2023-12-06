46de99a77aeb37c7050e55edc4dcc997929059c9
Joseph Gulay <joegulay@gmail.com>
2023-12-06 17:47:53 +1100

    Complete day 06

--- aoc_23/day_06.py
+++ aoc_23/day_06.py
@@ -1,16 +1,28 @@
 from load_data import get_data,get_test_data
-import re
 import numpy as np
-from itertools import chain
+
 day = 6
 """ Part 1 Pseudocode:
-- Find a way to parse the data such that there is a "Time" list and "Distance"
+1. Find a way to parse the data such that there is a "Time" list and "Distance"
 are lists within a dictionary, with a structure as follows:
 {'time' : [...], 'distance' : [...]}
     - Tests:
         - Time and Distance are lists
         - Lists are the same size
         - Lists contain positive integers
+
+2. Take in two numbers, the time and record distance.
+Create a reverse list loop that does a forward iteration and reverse
+iteration, multiiplying each of the numbers together.
+Then take look through the loop to see how many winning combinations there are
+where the distance is greater than the threshold
+    - Tests:
+        - Check integer
+        - Check with 7, 9, should return 4
+"""
+
+""" Part 2 Pseudocode:
+Join the string as one list, then let it run
 """
 
 def _positive_int(input_list: list) -> bool:
@@ -43,22 +55,65 @@ def _generate_lists(data:str) -> dict:
 
     return card_dict
 
-def part_1():
-    pass
+def _num_wins(time: int, distance_record: int) -> int:
+    """Takes in the time and distance record, and returns number of winning
+    combinations
+    """
+    # create race list
+    race_list = list(range(0, time+1))
+    distance_list = [race_list[i] * race_list[len(race_list)-1-i] for i in race_list]
+    winning_combinations = 0
+    for j in distance_list:
+        if j>distance_record:
+            winning_combinations +=1
 
-def part_2():
-    pass
+    return winning_combinations
+
+def _convert(num_list:list) -> list:
+    """Takes in a separate num_list and return a final int list
+    """
+    # Converting integer list to string list
+    s = [str(i) for i in num_list]
+
+    # Join list items using join()
+    res = int("".join(s).strip())
+
+    return [res]
+
+def part_1(data: str) -> np.int64:
+    """ Takes in the data, then returns number of way race could be won
+    """
+    race_data = _generate_lists(data)
+    time = race_data['time']
+    distance = race_data['distance']
+
+    combo_list = [_num_wins(time[i], distance[i]) for i in range(len(time))]
+
+    return np.prod(combo_list)
+
+def part_2(data:str) -> np.int64:
+    """ Takes in the data, joins the race number, then returns number of way race could be won
+    """
+    race_data = _generate_lists(data)
+    time = _convert(race_data['time'])
+    distance = _convert(race_data['distance'])
+
+    combo_list = [_num_wins(time[i], distance[i]) for i in range(len(time))]
+
+    return np.prod(combo_list)
 
 
 if __name__ == "__main__":
 
+    ## Build the data
+    # data = get_test_data(day)
+    # _generate_lists(data)
+    # _num_wins(7,9)
+
     ## Uncomment the lines below when your function passes the test!
-    data = get_test_data(day)
-    _generate_lists(data)
-    ## Uncomment the lines below when your function passes the test!
-    # data = get_data(day)
-    # print(f'part 1 solution = {part_1(real_data)}')
-    # print(f'part 2 solution = {part_2(real_data)}')
+    real_data = get_data(day)
+    print(f'part 1 solution = {part_1(real_data)}')
+    print(f'part 2 solution = {part_2(real_data)}')
 
 
     pass
--- tests/test_day_06.py
+++ tests/test_day_06.py
@@ -1,4 +1,4 @@
-from aoc_23.day_06 import _generate_lists, _positive_int
+from aoc_23.day_06 import _generate_lists, _positive_int, _num_wins, part_1, part_2
 from aoc_23.load_data import get_test_data
 import numpy as np
 
@@ -6,7 +6,7 @@ import numpy as np
 test_data = get_test_data(6)
 race_data = _generate_lists(test_data)
 expected_result_part_1 = 288
-expected_result_part_2 = None
+expected_result_part_2 = 71503
 
 
 def test_part_1():
@@ -18,8 +18,16 @@ def test_part_1():
     assert type(race_data['distance']) == list
     assert _positive_int(race_data['distance'])== True
 
+    assert type(_num_wins(7,9)) == int
+    assert _num_wins(7,9) == 4
+
+    assert type(part_1(test_data)) == np.int64
+    assert part_1(test_data) == expected_result_part_1
+
     pass
 
 
 def test_part_2():
-   pass
+    assert type(part_2(test_data)) == np.int64
+    assert part_2(test_data) == expected_result_part_2
+    pass
