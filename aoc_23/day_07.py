c4be813ec80685be2fa6c1ef88f1f142455a10ca
Joe Gulay <joegulay@gmail.com>
2023-12-08 06:41:03 +1100

    Add pseudocode for day 7

--- aoc_23/day_07.py
+++ aoc_23/day_07.py
@@ -2,8 +2,21 @@ from load_data import get_data,get_test_data
 import re
 import numpy as np
 from itertools import chain
-day = 'xx'
-
+day = 7
+""" Part 1 Pseudocode:
+1. Categorise a five letter string into a hand type based on the following logic:
+- All numbers are the same
+- 4 numbers are the same
+- 3 numbers and two numbers are the same
+- 3 numbers are the same
+- 2 numbers and 2 numbers are the same 
+- 2 numbers are the samep
+- no numbers are tbe same
+- 
+2. Find some way of accounting for each of the hand ranks. Maybe a weighting factor?
+2. Assign values to each of the strings where A = 14 and 2 = 2
+3. Apply weighting factor based on type of hand and calculate score
+"""
 def part_1():
     pass
 
