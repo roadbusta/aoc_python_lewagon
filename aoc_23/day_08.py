8f6535545df2896461a728d9cbd8f860db898ba9
Joe Gulay <joegulay@gmail.com>
2023-12-08 16:28:50 +1100

    Create and pass test for parsing the data

--- aoc_23/day_08.py
+++ aoc_23/day_08.py
@@ -17,6 +17,32 @@ def _parse_data(data:str )-> dict:
         }
     }
     """
+    raw_list = data.strip().split('\n\n')
+
+    # Create a data dictionary
+    card_dict = {}
+    card_dict['instructions'] = raw_list[0]
+    card_dict['network'] = {}
+    node_str_list = raw_list[1].strip().split('\n')
+    
+    for node_str in node_str_list:
+      
+        node = node_str[0:2]
+        node_left = node_str[7:9]
+        node_right = node_str[12:14]
+        card_dict['network'][node] = (node_left, node_right)
+    
+    # for node in raw_list[1]:
+    #     colon_idx = line.find(':') # Find the colon index
+    #     category = line[:colon_idx].strip().lower() # Find the full card
+    #     hist_data = [int(i) for i in line[colon_idx+1:].split()] # Get the historical data
+
+    #     card_dict[category] = hist_data # Populate the card dictionary
+
+    # return card_dict
+    # pass
+    print(card_dict)
+    print(node_str)
     pass
 
 def part_1():
@@ -28,12 +54,10 @@ def part_2():
 
 if __name__ == "__main__":
 
-    #  data = get_test_data(day)
+     data = get_test_data(day)
+     _parse_data(data)
 
     ## Uncomment the lines below when your function passes the test!
     # data = get_data(day)
     # print(f'part 1 solution = {part_1(real_data)}')
     # print(f'part 2 solution = {part_2(real_data)}')
-
-
-    pass
--- tests/test_day_08.py
+++ tests/test_day_08.py
@@ -3,7 +3,7 @@ from aoc_23.load_data import get_test_data
 import numpy as np
 
 
-test_data = get_test_data(08)
+test_data = get_test_data(8)
 expected_result_part_1 = None
 expected_result_part_2 = None
 
@@ -12,6 +12,8 @@ def test_part_1():
     assert type(test_data) == str
     assert type(_parse_data(test_data)) == dict
     assert len(_parse_data(test_data)) == 2
+    assert type(_parse_data(test_data)['network']['AAA'])== tuple
+
     pass
 
 
