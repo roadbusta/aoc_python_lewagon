49c75e81eb4622d8d8342d6735b2389226863e01
Joe Gulay <joegulay@gmail.com>
2023-12-08 17:34:21 +1100

    Update with working code, however it takes a long time to run

--- aoc_23/day_08.py
+++ aoc_23/day_08.py
@@ -1,7 +1,6 @@
 from load_data import get_data,get_test_data
-import re
-import numpy as np
-from itertools import chain
+from itertools import cycle
+import copy
 day = 8
 """Part 1 Pseudo Code
 1. Create function that outputs dictionary for instructions, and mapping dictionary
@@ -20,33 +19,45 @@ def _parse_data(data:str )-> dict:
     raw_list = data.strip().split('\n\n')
 
     # Create a data dictionary
-    card_dict = {}
-    card_dict['instructions'] = raw_list[0]
-    card_dict['network'] = {}
+    data_dict = {}
+    data_dict['instructions'] = raw_list[0]
+    data_dict['network'] = {}
     node_str_list = raw_list[1].strip().split('\n')
     
     for node_str in node_str_list:
       
-        node = node_str[0:2]
-        node_left = node_str[7:9]
-        node_right = node_str[12:14]
-        card_dict['network'][node] = (node_left, node_right)
+        node = node_str[0:3]
+        node_left = node_str[7:10]
+        node_right = node_str[12:15]
+        data_dict['network'][node] = (node_left, node_right)
     
-    # for node in raw_list[1]:
-    #     colon_idx = line.find(':') # Find the colon index
-    #     category = line[:colon_idx].strip().lower() # Find the full card
-    #     hist_data = [int(i) for i in line[colon_idx+1:].split()] # Get the historical data
+    return data_dict
 
-    #     card_dict[category] = hist_data # Populate the card dictionary
 
-    # return card_dict
-    # pass
-    print(card_dict)
-    print(node_str)
-    pass
+def part_1(data:str) -> dict:
+    """Takes in an original data string and finds the number of steps required
+    to get to the end of the map
+    """
+    #Read in the data
+    data_dict = _parse_data(data)
+    instructions = data_dict['instructions']
+    network = data_dict['network']
 
-def part_1():
-    pass
+    current_location = 'AAA'
+    count = 0
+    while current_location != 'ZZZ':
+        for step in cycle(instructions):
+            if step == 'L':
+                new_location = network[current_location][0]
+            else:
+                new_location = network[current_location][0]
+            count +=1
+            current_location = new_location
+            if count%10000000 ==0:
+                print(count)
+                print(current_location)
+
+    return count
 
 def part_2():
     pass
@@ -54,10 +65,75 @@ def part_2():
 
 if __name__ == "__main__":
 
-     data = get_test_data(day)
-     _parse_data(data)
+    # data = get_test_data(day)
+    # _parse_data(data)
 
     ## Uncomment the lines below when your function passes the test!
-    # data = get_data(day)
+    real_data = get_data(day)
     # print(f'part 1 solution = {part_1(real_data)}')
-    # print(f'part 2 solution = {part_2(real_data)}')
+    # # print(f'part 2 solution = {part_2(real_data)}')
+
+    data_dict = _parse_data(real_data)
+    instructions = data_dict['instructions']
+    network = data_dict['network']
+
+    print(f'Original size {len(network)}')
+    
+   # Create replacement dictionary
+    replacement_dict = {}
+    for key, value in network.items():
+        if value[0] == value[1]:
+            replacement_dict[key] = value
+
+    new_network = copy.deepcopy(network)
+    # Delete old keys
+    for key, value in network.items():
+        if key in replacement_dict.keys():
+            del new_network[key]
+
+    # Cycle through and replace values in dictionary
+    for key, value in new_network.items():
+        if value[0] in replacement_dict.keys():
+            new_l = replacement_dict[value[0]]
+        else:
+            new_l = value[0]
+        
+        if value[1] in replacement_dict.keys():
+            new_r = replacement_dict[value[1]]
+        else:
+            new_r = value[1]
+
+        new_network[key] = (new_l, new_r)
+    
+    print(f"Cycle 1 complete. Size:{len(new_network)}")
+    
+    network = copy.deepcopy(new_network)
+   # Create replacement dictionary
+    replacement_dict = {}
+    for key, value in network.items():
+        if value[0] == value[1]:
+            replacement_dict[key] = value
+   
+    new_network = copy.deepcopy(network)
+    # Delete old keys
+    for key, value in network.items():
+        if key in replacement_dict.keys():
+            del new_network[key]
+
+    # Cycle through and replace values in dictionary
+    for key, value in new_network.items():
+        if value[0] in replacement_dict.keys():
+            new_l = replacement_dict[value[0]]
+        else:
+            new_l = value[0]
+        
+        if value[1] in replacement_dict.keys():
+            new_r = replacement_dict[value[1]]
+        else:
+            new_r = value[1]
+
+        new_network[key] = (new_l, new_r)
+    
+    print(f"Cycle 2 complete. Size:{len(new_network)}")
+
+    
--- tests/test_day_08.py
+++ tests/test_day_08.py
@@ -1,10 +1,12 @@
-from aoc_23.day_08 import _parse_data
+from aoc_23.day_08 import _parse_data, part_1
 from aoc_23.load_data import get_test_data
-import numpy as np
+
 
 
 test_data = get_test_data(8)
-expected_result_part_1 = None
+test_data_2 = get_test_data('8-2')
+expected_result_part_1a = 2
+expected_result_part_1b = 6
 expected_result_part_2 = None
 
 
@@ -13,6 +15,9 @@ def test_part_1():
     assert type(_parse_data(test_data)) == dict
     assert len(_parse_data(test_data)) == 2
     assert type(_parse_data(test_data)['network']['AAA'])== tuple
+
+    assert part_1(test_data)== expected_result_part_1a
+    assert part_1(test_data_2) == expected_result_part_1b
 
     pass
 
