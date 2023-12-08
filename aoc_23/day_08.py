8897d6a27406a8369dbd231f4127462f593e8845
Joseph Gulay <joegulay@gmail.com>
2023-12-08 22:21:44 +1100

    brute force method passes tests

--- aoc_23/day_08.py
+++ aoc_23/day_08.py
@@ -5,10 +5,17 @@ day = 8
 """Part 1 Pseudo Code
 1. Create function that outputs dictionary for instructions, and mapping dictionary
 2. Iterate through dictionary to find next step in the loop
+
+Note: This method is brute force and takes too long.
+
+Will try a different approach.
+
+1. Find the terminal point achieved if the instructions are followed exactly once
+2. If ZZZ is reached, flag how many steps are taken for it to be reached
 """
 def _parse_data(data:str )-> dict:
     """Takes in the data as a string and returns a dictionary with the following structure:
-    { 
+    {
         'instructions' : [L,R...],
         'network' :
         {
@@ -23,18 +30,17 @@ def _parse_data(data:str )-> dict:
     data_dict['instructions'] = raw_list[0]
     data_dict['network'] = {}
     node_str_list = raw_list[1].strip().split('\n')
-    
+
     for node_str in node_str_list:
-      
+
         node = node_str[0:3]
         node_left = node_str[7:10]
         node_right = node_str[12:15]
         data_dict['network'][node] = (node_left, node_right)
-    
+
     return data_dict
 
-
-def part_1(data:str) -> dict:
+def _brute_force(data:str) -> dict:
     """Takes in an original data string and finds the number of steps required
     to get to the end of the map
     """
@@ -45,19 +51,61 @@ def part_1(data:str) -> dict:
 
     current_location = 'AAA'
     count = 0
-    while current_location != 'ZZZ':
-        for step in cycle(instructions):
+
+    for step in cycle(instructions):
+        if step == 'L':
+            new_location = network[current_location][0]
+        else:
+            new_location = network[current_location][1]
+        count +=1
+        current_location = new_location
+        if count%10000000 ==0:
+            print(count)
+            print(current_location)
+        if current_location == 'ZZZ':
+            break
+
+    return count
+
+def _find_next_location() -> str:
+    """Takes in instructions, netowrk, start and returns the next location"""
+
+def _terminal_dict(instructions: str, network:dict, start:str, end_search = False, end: str = 'ZZZ')-> dict:
+    """Takes in the instructions, network and starting point and returns a
+    dictionary containing the ending node. If end search is True,  """
+    if end_search:
+        record_count = None
+        count = 0
+        current_location = start
+        for step in instructions:
             if step == 'L':
                 new_location = network[current_location][0]
             else:
-                new_location = network[current_location][0]
-            count +=1
+                new_location = network[current_location][1]
+            count += 1
             current_location = new_location
-            if count%10000000 ==0:
-                print(count)
-                print(current_location)
+            if current_location == 'ZZZ':
+                record_count = count
+                break
+        terminal_dict = {'terminal' : current_location, 'ZZZ' : record_count}
 
-    return count
+    else:
+        current_location = start
+        for step in instructions:
+            if step == 'L':
+                new_location = network[current_location][0]
+            else:
+                new_location = network[current_location][1]
+            current_location = new_location
+
+        terminal_dict = {'terminal' : current_location, 'ZZZ' : None}
+
+    return terminal_dict
+
+
+
+def part_1(data:str) -> dict:
+    return _brute_force(data)
 
 def part_2():
     pass
@@ -67,73 +115,80 @@ if __name__ == "__main__":
 
     # data = get_test_data(day)
     # _parse_data(data)
-
-    ## Uncomment the lines below when your function passes the test!
+    test_data = get_test_data(8)
+    # data_dict = _parse_data(test_data)
+    # instructions = data_dict['instructions']
+    # network = data_dict['network']
+    # _terminal_dict(instructions, network, 'DDD', end_search=True)
+    # print(part_1(test_data))
+    # Uncomment the lines below when your function passes the test!
     real_data = get_data(day)
-    # print(f'part 1 solution = {part_1(real_data)}')
-    # # print(f'part 2 solution = {part_2(real_data)}')
+    print(f'part 1 solution = {part_1(real_data)}')
+    # # # print(f'part 2 solution = {part_2(real_data)}')
 
-    data_dict = _parse_data(real_data)
-    instructions = data_dict['instructions']
-    network = data_dict['network']
+    # data_dict = _parse_data(real_data)
+    # instructions = data_dict['instructions']
+    # network = data_dict['network']
 
-    print(f'Original size {len(network)}')
-    
-   # Create replacement dictionary
-    replacement_dict = {}
-    for key, value in network.items():
-        if value[0] == value[1]:
-            replacement_dict[key] = value
 
-    new_network = copy.deepcopy(network)
-    # Delete old keys
-    for key, value in network.items():
-        if key in replacement_dict.keys():
-            del new_network[key]
 
-    # Cycle through and replace values in dictionary
-    for key, value in new_network.items():
-        if value[0] in replacement_dict.keys():
-            new_l = replacement_dict[value[0]]
-        else:
-            new_l = value[0]
-        
-        if value[1] in replacement_dict.keys():
-            new_r = replacement_dict[value[1]]
-        else:
-            new_r = value[1]
 
-        new_network[key] = (new_l, new_r)
-    
-    print(f"Cycle 1 complete. Size:{len(new_network)}")
-    
-    network = copy.deepcopy(new_network)
-   # Create replacement dictionary
-    replacement_dict = {}
-    for key, value in network.items():
-        if value[0] == value[1]:
-            replacement_dict[key] = value
-   
-    new_network = copy.deepcopy(network)
-    # Delete old keys
-    for key, value in network.items():
-        if key in replacement_dict.keys():
-            del new_network[key]
 
-    # Cycle through and replace values in dictionary
-    for key, value in new_network.items():
-        if value[0] in replacement_dict.keys():
-            new_l = replacement_dict[value[0]]
-        else:
-            new_l = value[0]
-        
-        if value[1] in replacement_dict.keys():
-            new_r = replacement_dict[value[1]]
-        else:
-            new_r = value[1]
+#     print(f'Original size {len(network)}')
 
-        new_network[key] = (new_l, new_r)
-    
-    print(f"Cycle 2 complete. Size:{len(new_network)}")
+#    # Create replacement dictionary
+#     replacement_dict = {}
+#     for key, value in network.items():
+#         if value[0] == value[1]:
+#             replacement_dict[key] = value
 
-    
+#     new_network = copy.deepcopy(network)
+#     # Delete old keys
+#     for key, value in network.items():
+#         if key in replacement_dict.keys():
+#             del new_network[key]
+
+#     # Cycle through and replace values in dictionary
+#     for key, value in new_network.items():
+#         if value[0] in replacement_dict.keys():
+#             new_l = replacement_dict[value[0]]
+#         else:
+#             new_l = value[0]
+
+#         if value[1] in replacement_dict.keys():
+#             new_r = replacement_dict[value[1]]
+#         else:
+#             new_r = value[1]
+
+#         new_network[key] = (new_l, new_r)
+
+#     print(f"Cycle 1 complete. Size:{len(new_network)}")
+
+#     network = copy.deepcopy(new_network)
+#    # Create replacement dictionary
+#     replacement_dict = {}
+#     for key, value in network.items():
+#         if value[0] == value[1]:
+#             replacement_dict[key] = value
+
+#     new_network = copy.deepcopy(network)
+#     # Delete old keys
+#     for key, value in network.items():
+#         if key in replacement_dict.keys():
+#             del new_network[key]
+
+#     # Cycle through and replace values in dictionary
+#     for key, value in new_network.items():
+#         if value[0] in replacement_dict.keys():
+#             new_l = replacement_dict[value[0]]
+#         else:
+#             new_l = value[0]
+
+#         if value[1] in replacement_dict.keys():
+#             new_r = replacement_dict[value[1]]
+#         else:
+#             new_r = value[1]
+
+#         new_network[key] = (new_l, new_r)
+
+#     print(f"Cycle 2 complete. Size:{len(new_network)}")
--- tests/test_day_08.py
+++ tests/test_day_08.py
@@ -1,10 +1,13 @@
-from aoc_23.day_08 import _parse_data, part_1
+from aoc_23.day_08 import _parse_data, part_1, _terminal_dict
 from aoc_23.load_data import get_test_data
 
 
 
 test_data = get_test_data(8)
 test_data_2 = get_test_data('8-2')
+data_dict = _parse_data(test_data)
+instructions = data_dict['instructions']
+network = data_dict['network']
 expected_result_part_1a = 2
 expected_result_part_1b = 6
 expected_result_part_2 = None
@@ -12,13 +15,24 @@ expected_result_part_2 = None
 
 def test_part_1():
     assert type(test_data) == str
-    assert type(_parse_data(test_data)) == dict
-    assert len(_parse_data(test_data)) == 2
-    assert type(_parse_data(test_data)['network']['AAA'])== tuple
+    assert type(data_dict) == dict
+    assert len(data_dict) == 2
+    assert type(data_dict['network']['AAA'])== tuple
+
+
+    assert type(_terminal_dict(instructions, network, 'AAA')) == dict
+    assert _terminal_dict(instructions, network, 'AAA')['terminal'] == 'ZZZ'
+    assert _terminal_dict(instructions, network, 'DDD')['terminal'] == 'DDD'
+    assert _terminal_dict(instructions, network, 'BBB')['terminal'] == 'EEE'
+    assert _terminal_dict(instructions, network, 'AAA', end_search=True)['terminal'] == 'ZZZ'
+    assert _terminal_dict(instructions, network, 'DDD', end_search=True)['terminal'] == 'DDD'
+    assert _terminal_dict(instructions, network, 'BBB', end_search=True)['terminal'] == 'EEE'
+    assert _terminal_dict(instructions, network, 'AAA', end_search=True)['ZZZ'] == 2
+    assert _terminal_dict(instructions, network, 'DDD', end_search=True)['ZZZ'] == None
+    assert _terminal_dict(instructions, network, 'BBB', end_search=True)['ZZZ'] == None
 
     assert part_1(test_data)== expected_result_part_1a
     assert part_1(test_data_2) == expected_result_part_1b
-
     pass
 
 
