b367353aaf39f48220f9d113d065bb75e7dbb050
roadbusta <53895960+roadbusta@users.noreply.github.com>
2024-10-28 13:08:04 +0000

    Solved day 9

--- aoc_23/day_09.py
+++ aoc_23/day_09.py
@@ -35,11 +35,11 @@ def _history(int_list):
     return history
 
 def _history_list(int_list):
-    sum_hist = 1
+    sum_hist = True
     history_list = [int_list]
-    while sum_hist > 0:
+    while sum_hist:
         history_list.append(_history(history_list[-1]))
-        sum_hist = sum(history_list[-1])
+        sum_hist = any(history_list[-1])
     return history_list
 
 def _next_num(int_list):
@@ -49,12 +49,24 @@ def _next_num(int_list):
         next_num += i[-1]
     return next_num
 
+# troubleshoot
+def _concat(int_list_of_lists):
+    solutions = [ i[-1] for i in int_list_of_lists]
+    concat_list = [j[0:-1] for j in int_list_of_lists]
+    return concat_list, solutions
+
 
 def part_1(data):
     int_list_of_lists = _string_to_list(data)
     extrapolated_value = 0
+    count = 0
     for i in int_list_of_lists:
         extrapolated_value += _next_num(i)
+        count += 1
+        if count > 198:
+            print(count)
+            print(extrapolated_value)
+            print('\n')
 
     return extrapolated_value
 
@@ -64,7 +76,26 @@ def part_2():
 
 if __name__ == "__main__":
 
+ 
+    int_list_test = [0, 1, 1, 0]
+    print(_history_list(int_list_test))
+    print(_next_num(int_list_test))
+    print('\n')
 
+    # test_data = get_test_data(day)
+    # # test_data = get_data(day)
+    # int_list_of_lists = _string_to_list(test_data)
+    # concat_list, solutions = _concat(int_list_of_lists)
+    # for i in range(len(concat_list)):
+    #     try:
+    #         if _next_num(concat_list[i]) != solutions[i]:
+    #             print(_next_num(concat_list[i]))
+    #             print(solutions[i])
+    #             print(concat_list[i]) 
+    #     except:
+    #         print('error')
+
+    # print(f'part 1 test solution = {part_1(test_data)}')
     ## Uncomment the lines below when your function passes the test!
     start = time.time()
 
@@ -72,7 +103,7 @@ if __name__ == "__main__":
     real_data = get_data(day)
     print(f'part 1 solution = {part_1(real_data)}')
     end = time.time()
-    print(end - start)
+    # print(end - start)
     # print(f'part 2 solution = {part_2(real_data)}')
 
 
